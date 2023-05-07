1b75d059e0de8dbc0192a485da5e7e420c9b20d8
Asiak <asiakemp24@gmail.com>
2023-05-07 16:58:28 -0400

    created basic html structure and just created files for JS and CSS

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// Variables that may be needed
+var startButton = 
+// function for the start button
+// function for the timer and time subtracted and game over
+// function for questions
+// function for saving initials and score
